6f1a8af8c7670cb2d5297ba90414884a46e94d17
leninjunior <leninbessa@gmail.com>
2023-09-12 19:58:17 -0400

    feat: Foi criado o método salvar usuário no UsuarioController.

--- demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/service/UsuarioService.java
+++ demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/service/UsuarioService.java
@@ -1,10 +1,12 @@
 package com.lenin.demoparkapi.service;
 
 
+import com.lenin.demoparkapi.entity.Usuario;
 import com.lenin.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,8 @@ public class UsuarioService {
     private  final UsuarioRepository usuarioRepository;
 
 
-
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
-package com.lenin.demoparkapi.web.controller
+package com.lenin.demoparkapi.web.controller;
 
+import com.lenin.demoparkapi.entity.Usuario;
 import com.lenin.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,10 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
 
+        @PostMapping
+        public ResponseEntity<Usuario> create (Usuario usuario){
+            Usuario user = usuarioService.salvar(usuario);
+            return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+        }
 
 }
