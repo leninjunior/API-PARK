f4114101c281b4e9a2a4ae54484afd6e87b12075
leninjunior <leninbessa@gmail.com>
2023-09-14 18:15:46 -0400

    feat: Construído classes DTOs para dar mais segurança para  entidade. feat: Foram criados métodos de validações de senhas no service. feat: Foram criado métodos para buscar por ID.

---
+++ demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/web/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.lenin.demoparkapi.web.controller;
+
+import com.lenin.demoparkapi.entity.Usuario;
+import com.lenin.demoparkapi.service.UsuarioService;
+import com.lenin.demoparkapi.web.dto.UsuarioCreateDto;
+import com.lenin.demoparkapi.web.dto.UsuarioResponseDto;
+import com.lenin.demoparkapi.web.dto.UsuarioSenhaDto;
+import com.lenin.demoparkapi.web.dto.mapper.UsuarioMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+        Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(UsuarioMapper.toListDto(users));
+    }
+}
