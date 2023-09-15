0cd0ddcac305a4cabbee7684f3972e677778e5bd
leninjunior <leninbessa@gmail.com>
2023-09-15 19:36:49 -0400

    feat: Foi criado classe para tratar exceptions específica.

---
+++ demo-park-api/demo-park-api/src/main/java/com/lenin/demoparkapi/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.lenin.demoparkapi.web.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // vai ficar escutando esssa exception, toda vez que ela for lançada, vai cair neste método.
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                        HttpServletRequest request,
+                                                                        BindingResult result){
+        log.error("API ERROR ->", ex);
+    return ResponseEntity
+            .status(HttpStatus.UNPROCESSABLE_ENTITY)//STATUS ONDE APLICATION NÃO CONSEGUE PROCESSAR O OBJETO QUE FOI ENVIADO.
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campos inválidos", result) );
+    }
+
+}
